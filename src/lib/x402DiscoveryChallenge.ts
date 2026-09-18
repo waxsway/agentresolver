17b11194ef242c89f15e7028ecd2063838dd60ed
waxsway <waxsway@gmail.com>
2026-09-17 17:08:42 -0700

    Share buyer recovery in discovery challenges

--- src/lib/x402DiscoveryChallenge.ts
+++ src/lib/x402DiscoveryChallenge.ts
@@ -4,6 +4,7 @@ import { getPaidCapability, type PaidCapabilityId } from "@/lib/paidCapabilities
 import { X402_PREFLIGHT_OUTPUT_EXAMPLE, X402_PREFLIGHT_OUTPUT_SCHEMA } from "@/lib/x402PreflightDiscovery";
 import { X402_PING_OUTPUT_EXAMPLE, X402_PING_OUTPUT_SCHEMA } from "@/lib/x402PingDiscovery";
 import { x402WireResourceMetadata } from "@/lib/x402WireResourceMetadata";
+import { x402BuyerChallengeExtension } from "@/lib/x402BuyerChallengeExtension";
 import { X402_NETWORK, X402_PAY_TO, X402_SOLANA_ASSET, X402_SOLANA_FEE_PAYER, X402_SOLANA_NETWORK, X402_SOLANA_PAY_TO } from "@/lib/x402Config";
 
 const BASE_USDC = "0x833589fCD6eDb6E08f4c7C32D4f71b54bdA02913";
@@ -78,11 +79,7 @@ export function x402DiscoveryChallenge(
         bodyType: "json",
         output: discoveryOutput
       }),
-      agentresolver: {
-        capabilityId: config.id,
-        priceUsd: config.priceUsd,
-        costClass: config.costClass
-      }
+      agentresolver: x402BuyerChallengeExtension(capabilityId)
     }
   };
 
